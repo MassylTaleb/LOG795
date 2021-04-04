b6ff9c395671a10cf3e087bf671c061cb7eec69c
Massyl <massyltaleb@gmail.com>
2021-04-04 16:18:32 -0400

    fixed bug

--- ILS-SUMM-master/object_extraction/Extractor.py
+++ ILS-SUMM-master/object_extraction/Extractor.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import imutils
+import math
 
 
 class Extractor:
@@ -42,30 +44,51 @@ class Extractor:
 
     def get_objects_from_image(self, image):
         # Read Image
-        
-        # image = cv2.imread(image_path)
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         original = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         # Apply filters
         blurred = cv2.GaussianBlur(image, (5, 5), 0)
         canny = cv2.Canny(blurred, 100, 255, 1)
-        kernel = np.ones((3,3),np.uint8)
+        kernel = np.ones((3, 3), np.uint8)
         dilate = cv2.dilate(canny, kernel, iterations=1)
 
         # Find contours
-        cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        cnts = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        cnts = imutils.grab_contours(cnts)
+        five_percent_best = math.ceil(0.05 * len(cnts))
+        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:five_percent_best]
+        image_number = 0
+        bigger_to_smaller_boxes = []
+        for c in cnts:
+            rect = cv2.boundingRect(c)
+            bigger_to_smaller_boxes.append(rect)
+
+        smaller_to_bigger_boxes = reversed(bigger_to_smaller_boxes)
+        smaller_to_bigger_boxes = list(smaller_to_bigger_boxes)
+        boxes = smaller_to_bigger_boxes.copy()
+        not_in = []
+
+        for i, s in enumerate(smaller_to_bigger_boxes):
+            for b in bigger_to_smaller_boxes:
+                # If top-left inner box corner is inside the bounding box
+                if s[0] > b[0] and s[1] > b[1]:
+                    if (b[0] + b[2]) > (s[0] + s[2]) and (b[1] + b[3]) > (s[1] + s[3]):
+                        if not_in.count(s) == 0:
+                            not_in.append(s)
+
+        for b in not_in:
+            boxes.remove(b)
 
         # Iterate thorugh contours and filter for ROI
-        image_number = 0
+
         ROIS = []
-        for c in cnts:
-            x,y,w,h = cv2.boundingRect(c)
-            cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
-            ROI = original[y:y+h, x:x+w]
+        for i in boxes:
+            x, y, w, h = i
+            cv2.rectangle(image, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            ROI = original[y:y + h, x:x + w]
             ROIS.append(ROI)
-            # cv2.imwrite("/home/ziz/school/LOG795/object_extraction/test/ROI_{}.png".format(image_number), ROI)
             image_number += 1
 
         return ROIS
