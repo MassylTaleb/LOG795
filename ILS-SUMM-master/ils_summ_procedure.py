20772033e1c5ede216f70f6faac9e568bb38b75a
Massyl <massyltaleb@gmail.com>
2021-04-11 11:05:22 -0400

    remove object extracted folders before extracting objets

--- ILS-SUMM-master/ils_summ_procedure.py
+++ ILS-SUMM-master/ils_summ_procedure.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import os, sys, glob,math
+import shutil
+
 import youtube_dl
 import pandas as pd 
 import numpy as np
@@ -58,61 +60,73 @@ if __name__ == '__main__':
     documents_df = pd.read_csv(documents)
     print(documents_df.head())
 
-    # extractor = Extractor()
+    extractor = Extractor()
 
-    # # 2. Recuperer les liens des video youtube.
-    # urls_videos = documents_df.url_noSubs.unique()
-    
-    # # df_frames_objects = pd.DataFrame(colums=['index_word', 'frames','objects'])
+    # 2. Recuperer les liens des video youtube.
+    urls_videos = documents_df.url_noSubs.unique()
 
-    # # 3. Recuperer le videos youtube a partir du lien
-    # for url in documents_df.url_noSubs.unique():
-    #     # print(url)
-               
-    #     first_row = documents_df.loc[documents_df['url_noSubs'] == url].iloc[0]
-    #     title = first_row.title
+    # df_frames_objects = pd.DataFrame(colums=['index_word', 'frames','objects'])
 
-    #     video_path = os.path.join(videos_folder,title+'.mp4')
+    # Clear object extracted folders
+    folder_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'objects_extracted')
+    for filename in os.listdir(folder_name):
+        file_path = os.path.join(folder_name, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
-    #     pre_processing.load_video(url, videos_folder, video_path, title)
+    # 3. Recuperer le videos youtube a partir du lien
+    for url in documents_df.url_noSubs.unique():
+        # print(url)
 
-    #     segmentation = pre_processing.ffprobe_shot_segmentation(videos_folder, title+'.mp4')
-    #     frames_shot = pre_processing.get_frames_shot(video_path)
+        first_row = documents_df.loc[documents_df['url_noSubs'] == url].iloc[0]
+        title = first_row.title
 
-    #     keySum_valueReal_dict = summarization.video_summarize(videos_sum_folder,video_path, 0.1,title+"_sum.mp4")
+        video_path = os.path.join(videos_folder,title+'.mp4')
+
+        pre_processing.load_video(url, videos_folder, video_path, title)
+
+        segmentation = pre_processing.ffprobe_shot_segmentation(videos_folder, title+'.mp4')
+        frames_shot = pre_processing.get_frames_shot(video_path)
+
+        keySum_valueReal_dict = summarization.video_summarize(videos_sum_folder,video_path, 0.1,title+"_sum.mp4")
 
 
-    #     # 4. Extraire les objects
-    #     for index, row in documents_df.loc[documents_df['url_noSubs'] == url].iterrows():
+        # 4. Extraire les objects
+        for index, row in documents_df.loc[documents_df['url_noSubs'] == url].iterrows():
 
-    #         object_extracted_title_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'objects_extracted',title)
-    #         if not os.path.exists(object_extracted_title_folder):os.makedirs(object_extracted_title_folder)
 
-    #         #Get start_time as total seconds
-    #         t_i = pd.to_datetime(row.start_time)
-    #         start_time = (timedelta(minutes = t_i.hour, seconds=t_i.minute, microseconds=t_i.second/0.00006)).total_seconds()
+            object_extracted_title_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'objects_extracted',title)
+            if not os.path.exists(object_extracted_title_folder):os.makedirs(object_extracted_title_folder)
+            #Get start_time as total seconds
+            t_i = pd.to_datetime(row.start_time)
+            start_time = (timedelta(minutes = t_i.hour, seconds=t_i.minute, microseconds=t_i.second/0.00006)).total_seconds()
 
-    #         #Get end_time as total seconds
-    #         t_f = pd.to_datetime(row.end_time)
-    #         end_time = (timedelta(minutes = t_f.hour, seconds=t_f.minute, microseconds=t_f.second/0.00006)).total_seconds()
+            #Get end_time as total seconds
+            t_f = pd.to_datetime(row.end_time)
+            end_time = (timedelta(minutes = t_f.hour, seconds=t_f.minute, microseconds=t_f.second/0.00006)).total_seconds()
 
-    #         # times_list = extractor.get_times_needed(keySum_valueReal_dict, start_time, end_time)
-    #         video_sum_path = os.path.join(videos_sum_folder, title+"_sum.mp4")
-    #         frames = extractor.get_multi_frames(keySum_valueReal_dict, start_time, end_time, video_sum_path)
+            # times_list = extractor.get_times_needed(keySum_valueReal_dict, start_time, end_time)
+            video_sum_path = os.path.join(videos_sum_folder, title+"_sum.mp4")
+            frames = extractor.get_multi_frames(keySum_valueReal_dict, start_time, end_time, video_sum_path)
 
-    #         frame_count = 0
-    #         for frame in frames:
+            frame_count = 0
+            for frame in frames:
 
-    #             objects = extractor.get_objects_from_image(frame)
+                objects = extractor.get_objects_from_image(frame)
 
-    #             object_count = 0
-    #             for roi in objects:
-    #                 object_name = str(index)+'_'+str(frame_count)+'_'+str(object_count)+'.png'
-    #                 t = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
-    #                 cv2.imwrite(os.path.join(object_extracted_title_folder, object_name), t)
-    #                 object_count += 1
-                    
-    #             frame_count+=1
+                object_count = 0
+                for roi in objects:
+                    object_name = str(index)+'_'+str(frame_count)+'_'+str(object_count)+'.png'
+                    t = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+                    cv2.imwrite(os.path.join(object_extracted_title_folder, object_name), t)
+                    object_count += 1
+
+                frame_count+=1
 
 
     print("\n\n------------------ CLUSTERING ---------------------")
@@ -221,7 +235,7 @@ if __name__ == '__main__':
 
     # Plotting result
     # -----------------------------------------------------------------------------------------
-    pdf = matplotlib.backends.backend_pdf.PdfPages('/home/ziz/school/LOG795/ILS-SUMM-master/data/rapport/test.pdf')
+    pdf = matplotlib.backends.backend_pdf.PdfPages('C:/Users/massy/Dev/LOG795/ILS-SUMM-master/data/rapport/test.pdf')
     clusters = np.sort(objects_df.cluster.unique())
 
     for cluster in clusters:
